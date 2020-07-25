9fc87d03a4f70bc3a8166411c6e634d6fd98abaf
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-25 18:24:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,10 +10,10 @@ class School
   end
 
   def add_student(student_name,grade)
-      if self.roster.none?{|current_grade| current_grade == grade}
-          self.roster[grade] = []
+      if .roster.none?{|current_grade| current_grade == grade}
+          .roster[grade] = []
         end
 
-    self.roster[grade] << student_name
+    .roster[grade] << student_name
   end
 end
